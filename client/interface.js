244c0fe66dc6dcbc31ef4cc79d88be05751c2dba
Mark Reale <reale.mark@gmail.com>
2012-12-18 16:04:55 -0500

    hide/show for great work
    ability to hit the appropriate button to trigger the appropriate input
    field / button for leaving great work

---
+++ client/interface.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$('body').on('click','.give_take .give',function(e){
+		e.preventDefault();
+		$(this).closest('.front_card').css({
+			display: 'none'
+		});
+		$thisComment = $(this).closest('.front_card').next();
+		$thisComment.css({
+			display: 'block'
+		});
+		$('.dec',$thisComment).hide();
+	});
+
+	$('body').on('click','.give_take .take',function(e){
+		e.preventDefault();
+		$(this).closest('.front_card').css({
+			display: 'none'
+		});
+		$thisComment = $(this).closest('.front_card').next();
+		$thisComment.css({
+			display: 'block'
+		});
+		$('.inc',$thisComment).hide();
+	});
+});
