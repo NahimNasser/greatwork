b25d46c2efc0ee38587ab68a5e12a5449d46a5b9
NahimNasser <nahim@bnotions.com>
2013-01-09 22:03:42 -0500

    recent actions widget

--- client/leaderboard.js
+++ client/leaderboard.js
@@ -22,7 +22,7 @@ if (Meteor.isClient) {
   Session.set('page_size', 3);
 
   Template.messages.messages = function() {
-    return Messages.find({}, {sort: {time: -1}}).fetch().slice(0,1);
+    return Messages.find({}, {sort: {time: -1}}).fetch();
   };
 
   Template.currentplayer.currentplayer = function(){
